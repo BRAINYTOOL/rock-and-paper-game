fe6e2390b464240dba901cb6ca81f5f49ccaefaf
BRAINYTOOL <binc4321@gmail.com>
2023-03-07 02:46:03 +0100

    rock-and-paper-game

--- script.js
+++ script.js
@@ -1,62 +1,79 @@
+let playerPoint = document.getElementById('plyScore').textContent = 0;
+let computerPoint = document.getElementById('compScore').textContent = 0;
 
-let playerPoint = 0
-let computerPoint = 0
 
-function playARound() { 
-    let playerSelect = prompt("please enter your symbol"," ");
-    let playerSelection = playerSelect.toUpperCase()
-    console.log (playerSelection)
-    function computerChoice () { 
-        let randomSelect = Math.floor(Math.random()*3) + 1
-        if (randomSelect=== 1) {return "ROCK"}
-        else if (randomSelect=== 2) {return "PAPER"}
-        else { return "SCISSORS"};}
- computerChoice ();
-    let getComputerChoice = computerChoice ()
+function playARound(value) {
+ 
+
+  let playerSelection = value
+console.log(playerSelection)
+function computerChoice () { 
+    let randomSelect = Math.floor(Math.random()*3) + 1
+    if (randomSelect=== 1) { return "ROCK"}
+    else if (randomSelect=== 2) {return "PAPER"}
+    else { return "SCISSORS"};}
+computerChoice ();
+let getComputerChoice = computerChoice()
+document.getElementById("comBtn").textContent = getComputerChoice
 if ( playerSelection === getComputerChoice) {
-  alert("That's a tie! Play another round")
-  playARound ()}
-  else if ( playerSelection === "ROCK" && getComputerChoice === "PAPER") {
-    alert("You lose! Paper beats Rock")
-  return computerPoint++}
-  else if ( playerSelection === "PAPER" && getComputerChoice === "ROCK") {
-    alert("You win! Paper beats Rock")
-    return playerPoint++}
-  else if ( playerSelection === "PAPER" && getComputerChoice === "SCISSORS") {
-    alert("You lose! Scissors beats paper")
-    return computerPoint++}
-  else if ( playerSelection === "SCISSORS" && getComputerChoice === "PAPER") {
-    alert("You win! Scissors beats paper")
-    return playerPoint++}
-  else if ( playerSelection === "ROCK" && getComputerChoice === "SCISSORS") {
-    alert("You win! Rock beats Scissors")
-    return playerPoint++}
-  else if ( playerSelection === "SCISSORS" && getComputerChoice === "ROCK") {
-    alert("You lose! Rock beats Scissors")
-    return computerPoint++}
-  else { 
-    alert("please enter correct symbols")
-    playARound ()};
+document.getElementById("comment").textContent = "That's a tie! Play another round"
 }
+else if ( playerSelection === "ROCK" && getComputerChoice === "PAPER") {
+document.getElementById("comment").textContent = "You lose! Paper beats Rock"
+computerPoint = computerPoint + 1;
+document.getElementById('compScore').textContent = computerPoint}
+else if ( playerSelection === "PAPER" && getComputerChoice === "ROCK") {
+document.getElementById("comment").textContent = "You win! Paper beats Rock"
+playerPoint = playerPoint + 1;
+document.getElementById('plyScore').textContent = playerPoint}
+else if ( playerSelection === "PAPER" && getComputerChoice === "SCISSORS") {
+document.getElementById("comment").textContent = "You lose! Scissors beats paper"
+computerPoint = computerPoint + 1;
+document.getElementById('compScore').textContent = computerPoint}
+else if ( playerSelection === "SCISSORS" && getComputerChoice === "PAPER") {
+document.getElementById("comment").textContent = "You win! Scissors beats paper"
+playerPoint = playerPoint + 1;
+document.getElementById('plyScore').textContent = playerPoint}
+else if ( playerSelection === "ROCK" && getComputerChoice === "SCISSORS") {
+document.getElementById("comment").textContent = "You win! Rock beats Scissors"
+playerPoint = playerPoint + 1;
+document.getElementById('plyScore').textContent = playerPoint}
+else if ( playerSelection === "SCISSORS" && getComputerChoice === "ROCK") {
+document.getElementById("comment").textContent = "You lose! Rock beats Scissors"
+computerPoint = computerPoint + 1;
+document.getElementById('compScore').textContent = computerPoint}
+checkWinner ();
+
+}
+
 
 
 function checkWinner () {
-  if (playerPoint < computerPoint) {
-    alert("You lost this game")
-  } else{
-    alert("Congratulations! 🏆 you won")
-  }
+if (playerPoint === 5) { openNav(); 
+document.getElementById("overlay-content").innerHTML ="Congratulations! You won"} 
+else if ( computerPoint === 5)
+{openNav();
+  document.getElementById("overlay-content").innerHTML ="Sorry you lost to Computer"
+} 
 }
 
-for (let i = 0; i < 5; i++) {
-playARound()
+
+openNav = () => {
+  document.getElementById("myNav").style.height = "100%";
 }
 
-function scoreBoard () {
-  alert(`Your points: ${playerPoint}
-Computer points: ${computerPoint}`)
-checkWinner()
 
-}
 
-scoreBoard()
+
+
+
+
+
+
+
+
+
+
+
+
+
